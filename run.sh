31fd9bac5224c47a0347dcc44c07c94e64f61597
Michal Walczak <magikmw@michalwalczak.eu>
2012-04-14 00:16:44 +0200

    changed the protocol to tcp/ip - socket required setting 777 to socket file, probably my fault

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-python manage.py runfcgi daemonize=false socket=/tmp/djangoblog.sock
+#python manage.py runfcgi daemonize=false socket=/tmp/djangoblog.sock
+python manage.py runfcgi daemonize=false host=127.0.0.1 port=3033
 
 # .sock 777
